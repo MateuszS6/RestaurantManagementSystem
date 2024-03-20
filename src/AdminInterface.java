52fbb9d9f62f5a1f475e89451025909e78724f8b
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2024-03-20 23:52:16 +0000

    Rearrange code

---
+++ src/AdminInterface.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class AdminInterface {
+    private JPanel mainPanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
+    public AdminInterface() {
+
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 560;
+    private JPanel screen;
+
+    public Main() {
+        // Window setup
+        JFrame window = new JFrame("Restaurant Management");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setVisible(true);
+        screen = new AdminInterface().getMainPanel();
+        screen.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        window.add(screen);
+        window.pack();
+        window.setLocationRelativeTo(null); // Centre on screen
+    }
+
     public static void main(String[] args) {
-        new UserInterface();
+        new Main();
     }
 }

69ed21b474fc009041ba6a2f97d8c5bc510c8375
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2024-03-20 23:25:53 +0000

    Standard window setup

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,2 +1,16 @@
+import javax.swing.*;
+
 public class UserInterface {
+    private JPanel mainPanel;
+
+    public UserInterface() {
+        // Window setup
+        JFrame window = new JFrame("Restaurant Management");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(1296, 759);
+        window.setLocationRelativeTo(null); // Centre on screen
+        window.setResizable(false);
+        window.setVisible(true);
+        window.add(mainPanel);
+    }
 }

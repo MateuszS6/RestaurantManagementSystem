2f5edde753b9c60e4bbbefd842063f5e23225793
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2024-03-21 23:09:52 +0000

    Bug fixes

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -9,23 +9,30 @@ import java.awt.event.FocusListener;
 import java.util.Arrays;
 
 public class LoginPage implements ActionListener, FocusListener, DocumentListener {
-    private Main main;
+    private final Color defaultTextColour = new Color(0xFF777777, true);
+    private final Main main;
+    private final char echoChar;
     private JPanel mainPanel;
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JButton clearButton;
     private JButton loginButton;
-    private final Color defaultTextColour = new Color(0xFF777777, true);
+    private JLabel error;
 
     public LoginPage(Main main) {
         this.main = main;
+        echoChar = passwordField.getEchoChar();
+        passwordField.setEchoChar((char) 0);
 
+        // To display default text hint in fields when empty
         usernameField.addFocusListener(this);
         passwordField.addFocusListener(this);
 
+        // To enable/disable buttons based on field input being empty/not
         usernameField.getDocument().addDocumentListener(this);
         passwordField.getDocument().addDocumentListener(this);
 
+        // To clear the fields or attempt log in
         loginButton.addActionListener(this);
         clearButton.addActionListener(this);
     }
@@ -48,16 +55,22 @@ public class LoginPage implements ActionListener, FocusListener, DocumentListene
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == clearButton) {
-            usernameField.setText("");
-            passwordField.setText("");
+            usernameField.setForeground(defaultTextColour);
+            usernameField.setText("Username");
+            passwordField.setForeground(defaultTextColour);
+            passwordField.setEchoChar((char) 0);
+            passwordField.setText("Password");
+            mainPanel.requestFocus();
+            error.setText(" ");
         } else if (e.getSource() == loginButton) {
             String username = usernameField.getText();
             String password = Arrays.toString(passwordField.getPassword());
             if (username.equals("mateusz") && password.equals("[m, a, t, z]")) {
-                System.out.println("Login successful");
                 main.switchPanel(new HomePage().getMainPanel());
+//                System.out.println("Login successful");
             } else {
-                System.out.println("Login unsuccessful");
+                error.setText("Incorrect username or password.");
+//                System.out.println("Login unsuccessful");
             }
         }
     }
@@ -70,6 +83,7 @@ public class LoginPage implements ActionListener, FocusListener, DocumentListene
         }
         if (e.getSource() == passwordField && passwordField.getText().equals("Password")) {
             passwordField.setForeground(Color.BLACK);
+            passwordField.setEchoChar(echoChar);
             passwordField.setText("");
         }
     }
@@ -82,6 +96,7 @@ public class LoginPage implements ActionListener, FocusListener, DocumentListene
         }
         if (e.getSource() == passwordField && passwordField.getText().isEmpty()) {
             passwordField.setForeground(defaultTextColour);
+            passwordField.setEchoChar((char) 0);
             passwordField.setText("Password");
         }
     }
